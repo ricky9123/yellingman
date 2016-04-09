ede44d880fb915e4f86608427ab1768638be78e2
Wangbicong <wangbicong@hotmail.com>
2016-04-09 11:00:55 +0800

    添加后台测试

--- restfulapi/config.py
+++ restfulapi/config.py
@@ -2,10 +2,12 @@ import os
 
 
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '1dc7e39bd2210b6051f98926e0eec2e7'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'mysql://wbc:0122@182.254.154.138:3306/yellingman'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     @staticmethod
     def init_app(app):
--- restfulapi/manage.py
+++ restfulapi/manage.py
@@ -10,9 +10,15 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, User=User, Music=Music, Record=Record)
+    return dict(app=app, db=db, User=User, Music=Music, Record=Record)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
